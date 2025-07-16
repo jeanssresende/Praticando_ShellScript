93f31752e38b1c0fd76502494fc40f6020455595
Jean Silva de Souza Resende <jean.resende@professor.fpp.edu.br>
2025-07-16 07:00:58 -0300

    update

---
+++ variaveis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Teste"
+
+NOME="Jean Resende"
+
+echo "$NOME"
+
+NUMERO_1=24
+NUMERO_2=45
+TOTAL=$(($NUMERO_1 + NUMERO_2))
+
+echo "A soma de $NUMERO_1 + $NUMERO_2 é igual a $TOTAL"
+
+
+#SAIDA_CAT=$(cat /etc/passwd | grep jean)
+#echo "A saída do comando cat é: $SAIDA_CAT"
